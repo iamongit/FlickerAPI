3e3ef3e2a3f9a3912f91b7081925da683cd18855
iamongit <ayousuf2@binghamton.edu>
2017-03-06 17:45:40 -0500

    links and titles acquired in scope

--- app/app.js
+++ app/app.js
@@ -8,20 +8,34 @@ angular.module('app', ['ngRoute'])
 	})
 })
 .controller('appController', function($scope, $http){
-	
-	$http({
+	$scope.tags = {},
+	$scope.links = [],
+	$scope.postRequest = function(){
+		$http({
   		method: 'POST',
-  		url: 'localhost:2000',
-  		data: { tag: $scope.requiredTag }
+  		url: 'http://localhost:2000/',
+  		headers: {
+   			'Content-Type': 'application/JSON'
+ 		},
+  		data: { tag: $scope.tags.requiredTag }
 		}).then(function successCallback(response) {
-    	// this callback will be called asynchronously
-    	// when the response is available
-    	console.log('post request successful')
+    	// console.log('post request successful response is', response.data)
+    	var obj = JSON.parse(response.data);
+    	for(var i=0;i<obj.photos.photo.length;i++){
+    		var temp = obj.photos.photo[i];
+    		var finLink = 'https://farm' + temp.farm + '.staticflickr.com/'+temp.server+'/'+temp.id+'_'+temp.secret+'.jpg';
+    		$scope.links.push({link: finLink,title:temp.title });
+    	}
+    	console.log('links', $scope.links);
   		}, function errorCallback(response) {
     	// called asynchronously if an error occurs
     	// or server returns response with an error status.
     	console.log('post request failed')
   		});
+  		$scope.tags.requiredTag = '';
+	}
 
 })
+
+//https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
 
--- server/server.js
+++ server/server.js
@@ -16,37 +16,50 @@ app.listen(app.get('port'), function(){
 });
 
 app.post('/',function(req,res){
-  var tag=req.body.tag;
-  console.log("Searched tag name = "+tag);
-  res.end("yes");
+	var options = { method: 'GET',
+	  url: 'https://api.flickr.com/services/rest/',
+	  qs:
+	   { method: 'flickr.photos.search',
+	     api_key: 'abf53b7689c97eb14f4f0e35c32cbb27',
+	     tags: 'timessquare',
+	     woe_id: '2459115',
+	     format: 'json',
+	     nojsoncallback: '1',
+	     auth_token: '72157681113405355-4a8536f965eb4533',
+	     api_sig: 'aa8c8fe2808794d0e01b523056ea898d' },
+	  headers:
+	   { 'postman-token': 'b625ac6b-e54b-c9ba-4a3f-24701f627f0f',
+	     'cache-control': 'no-cache',
+	     woe_id: '2459115',
+	     api_key: 'abf53b7689c97eb14f4f0e35c32cbb27' } 
+	 };
+ 
+  options.qs.tags = req.body.tag;
+  request(options, function (error, response, body) {
+	  if (error){
+	  	throw new Error(error);
+	  	console.log('error', error)
+	  }
+	  // console.log(body, 'body');
+	  res.json(body);
+  });
+
+  // res.end("yes");
 });
 
-var options = { method: 'GET',
-  url: 'https://api.flickr.com/services/rest/',
-  qs:
-   { method: 'flickr.photos.search',
-     api_key: 'abf53b7689c97eb14f4f0e35c32cbb27',
-     tags: 'timessquare',
-     woe_id: '2459115',
-     format: 'json',
-     nojsoncallback: '1',
-     auth_token: '72157681113405355-4a8536f965eb4533',
-     api_sig: 'aa8c8fe2808794d0e01b523056ea898d' },
-  headers:
-   { 'postman-token': 'b625ac6b-e54b-c9ba-4a3f-24701f627f0f',
-     'cache-control': 'no-cache',
-     woe_id: '2459115',
-     api_key: 'abf53b7689c97eb14f4f0e35c32cbb27' } };
 
-var result = [];
-request(options, function (error, response, body) {
-  if (error){
-  	throw new Error(error);
-  	console.log('error', error)
-  }
-  result.push(body);
-  console.log(result, 'resulttt');
-});
-// console.log(result, 'resulttt');
+
+// var result = [];
+// request(options, function (error, response, body) {
+//   if (error){
+//   	throw new Error(error);
+//   	console.log('error', error)
+//   }
+//   result.push(body);
+//   // console.log(result, 'resulttt');
+// });
+// // console.log(result, 'resulttt');
+
+
 
 module.exports.app = app;
